59cfc10b3c0b87ec6e4d813c73cdc655e2bcc3e3
alicebyers5 <alice.byers@gov.scot>
2023-02-06 18:19:43 +0000

    Use scale_colour/fill_*_sg as default and print info messages

--- R/use_sgplot.R
+++ R/use_sgplot.R
@@ -1,9 +1,11 @@
 #' @title Use \code{sgplot} defaults.
 #'
-#' @description Set \code{sgplot} theme and geom aesthetics as default for
-#' ggplot2 charts.
+#' @description Set \code{sgplot} theme, colour palette and geom aesthetic
+#' defaults for ggplot2 charts.
 #'
-#' @param default_col Default colour/fill for geoms. Default value is
+#' @param default_grid 'x', 'y', 'xy' or 'none' to determine which grid lines
+#' should be drawn. Defaults to 'y'.
+#' @param default_colour Default colour/fill for geoms. Default value is
 #' 'dark-blue' from \code{sgplot::sg_colour_values}.
 #'
 #' @examples
@@ -12,11 +14,24 @@
 #' @export
 
 
-use_sgplot <- function(default_col = sgplot::sg_colour_values["dark-blue"]) {
+use_sgplot <- function(default_grid = c("y", "x", "xy", "none"),
+                       default_colour = sgplot::sg_colour_values["dark-blue"]) {
 
   # Use sgplot theme ----
 
-  ggplot2::theme_set(theme_sg())
+  ggplot2::theme_set(theme_sg(grid = default_grid))
+
+  cli::cli_alert_info("Default ggplot2 theme set to `theme_sg`.")
+
+
+  # Use sgplot colour palette ----
+
+  options(ggplot2.continuous.fill = scale_fill_continuous_sg,
+          ggplot2.continuous.colour = scale_colour_continuous_sg,
+          ggplot2.discrete.fill = scale_fill_discrete_sg,
+          ggplot2.discrete.colour = scale_colour_discrete_sg)
+
+  cli::cli_alert_info("Default colours set.")
 
 
   # Set default geom characteristics ----
@@ -53,5 +68,8 @@ use_sgplot <- function(default_col = sgplot::sg_colour_values["dark-blue"]) {
     geom = "point",
     new = list(colour = default_col)
   )
+
+  cli::cli_alert_info("Default geom aesthetics set.")
+
 
 }
