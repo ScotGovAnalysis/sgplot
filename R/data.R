b70c33f3631a8b8da7c0920ec5ef0d4f0bd2a146
alicebyers5 <alice.byers@gov.scot>
2023-02-23 14:55:38 +0000

    Fix lint exclusions

--- R/data.R
+++ R/data.R
@@ -1,19 +1,23 @@
+# nolint start
 #' @title Scottish Government colour names and hex codes
 #'
 #' @description A vector containing colour names and their corresponding
 #' hex code.
 #'
 #' @format A character vector
-#' @source \href{https://analysisfunction.civilservice.gov.uk/policy-store/data-visualisation-colours-in-charts/}{Government Analysis Function Colours Guidance} # nolint
+#' @source \href{https://analysisfunction.civilservice.gov.uk/policy-store/data-visualisation-colours-in-charts/}{Government Analysis Function Colours Guidance}
+# nolint end
 
 "sg_colour_values"
 
 
+# nolint start
 #' @title Scottish Government colour palettes
 #'
 #' @description A list grouping colours into palettes.
 #'
 #' @format A character list
-#' @source \href{https://analysisfunction.civilservice.gov.uk/policy-store/data-visualisation-colours-in-charts/}{Government Analysis Function Colours Guidance} # nolint
+#' @source \href{https://analysisfunction.civilservice.gov.uk/policy-store/data-visualisation-colours-in-charts/}{Government Analysis Function Colours Guidance}
+# nolint end
 
 "sg_colour_palettes"
