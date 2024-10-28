cec06c27588540ef81bb75b27e6a0148aae877a4
alice-hannah <alice.hannah@gov.scot>
2024-10-28 16:44:31 +0000

    Combine docs for scale functions (closes #41)

--- R/scale_continuous_sg.R
+++ R/scale_continuous_sg.R
@@ -1,15 +1,10 @@
-#' @title Continuous colour scales for Scottish Government plots
+#' @title Continuous colour/fill scales for Scottish Government plots
 #'
 #' @param palette Name of palette to use; e.g. "main", "sequential", "focus".
 #' Default value is "sequential".
-#' @param palette_type Either `sg` to use Scottish Government palettes, `sss`
-#' to use Social Security Scotland palettes or `af` to use Analysis Function
-#' palettes. Defaults to `sg`.
-#' @param reverse Boolean value to indicate whether the palette should be
-#' reversed.
 #' @param na_colour Colour to set for missing values.
 #' @param guide A name or function used to create guide. Default is "colourbar".
-#' @param ... Additional arguments passed to scale type.
+#' @inheritParams scale_colour_discrete_sg
 #'
 #' @examples
 #' library(ggplot2)
@@ -17,6 +12,10 @@
 #' ggplot(mtcars, aes(x = mpg, y = wt, colour = cyl)) +
 #'   geom_point() +
 #'   scale_colour_continuous_sg()
+#'
+#' ggplot(faithfuld, aes(x = waiting, y = eruptions, fill = density)) +
+#'   geom_raster() +
+#'   scale_fill_continuous_sg()
 #'
 #' @export
 
@@ -48,6 +47,45 @@ scale_colour_continuous_sg <- function(palette = "sequential",
 
   ggplot2::continuous_scale(
     aesthetics = "colour",
+    scale_name = paste0(palette_type, "_continuous"),
+    palette    = scales::gradient_n_pal(colours, values = NULL, "Lab"),
+    na.value   = na_colour,
+    guide      = guide,
+    ...
+  )
+}
+
+#' @export
+#' @rdname scale_colour_continuous_sg
+
+scale_fill_continuous_sg <- function(palette = "sequential",
+                                     palette_type = c("sg", "sss", "af"),
+                                     reverse = FALSE,
+                                     na_colour = "grey50",
+                                     guide = "colourbar",
+                                     ...) {
+
+  palette_type <- match.arg(palette_type)
+
+  palette_list <- switch(
+    palette_type,
+    af  = sgplot::af_colour_palettes,
+    sg  = sgplot::sg_colour_palettes,
+    sss = sgplot::sss_colour_palettes
+  )
+
+  # Error if palette doesn't exist
+  if (!palette %in% names(palette_list)) {
+    cli::cli_abort(c(
+      "x" = paste("`{palette}` is not a valid palette name in",
+                  "`{palette_type}_colour_palettes`.")
+    ))
+  }
+
+  colours <- as.vector(palette_list[[palette]])
+
+  ggplot2::continuous_scale(
+    aesthetics = "fill",
     scale_name = paste0(palette_type, "_continuous"),
     palette    = scales::gradient_n_pal(colours, values = NULL, "Lab"),
     na.value   = na_colour,
---
+++ R/scale_discrete_sg.R
@@ -0,0 +1,64 @@
+#' @title Discrete colour/fill scales for Scottish Government plots
+#'
+#' @param palette Name of palette to use; e.g. "main", "sequential", "focus".
+#' Default value is "main".
+#' @param palette_type Either "sg" to use Scottish Government palettes
+#' (default), "sss" to use Social Security Scotland palettes or "af" to use
+#' Analysis Function palettes.
+#' @param reverse Boolean value to indicate whether the palette should be
+#' reversed.
+#' @param ... Additional arguments passed to scale type.
+#'
+#' @examples
+#' library(ggplot2)
+#' library(dplyr)
+#'
+#' economics_long %>%
+#'   filter(variable %in% c("psavert", "uempmed")) %>%
+#'   ggplot(aes(x = date, y = value, colour = variable)) +
+#'   geom_line(linewidth = 1) +
+#'   scale_colour_discrete_sg()
+#'
+#' d <- subset(mpg, manufacturer == "ford")
+#'
+#' ggplot(d, aes(x = class, fill = class)) +
+#'  geom_bar() +
+#'  scale_fill_discrete_sg()
+#'
+#' @export
+
+scale_colour_discrete_sg <- function(palette = "main",
+                                     palette_type = c("sg", "sss", "af"),
+                                     reverse = FALSE,
+                                     ...) {
+
+  palette_type <- match.arg(palette_type)
+
+  ggplot2::discrete_scale(
+    "colour",
+    paste0(palette_type, "_", palette),
+    palette = sg_palette(palette, reverse, palette_type = palette_type),
+    ...
+  )
+
+}
+
+
+#' @export
+#' @rdname scale_colour_discrete_sg
+
+scale_fill_discrete_sg <- function(palette = "main",
+                                   palette_type = c("sg", "sss", "af"),
+                                   reverse = FALSE,
+                                   ...) {
+
+  palette_type <- match.arg(palette_type)
+
+  ggplot2::discrete_scale(
+    "fill",
+    paste0(palette_type, "_", palette),
+    palette = sg_palette(palette, reverse, palette_type = palette_type),
+    ...
+  )
+
+}
