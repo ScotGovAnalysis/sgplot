c5cd9699f1caab51408dc850ff2c4295f4349a58
alice-hannah <alice.hannah@gov.scot>
2025-05-22 10:29:13 +0100

    Use dplyr pipe instead of base pipe.

--- R/colour_table.R
+++ R/colour_table.R
@@ -9,14 +9,16 @@
 
 colour_table <- function(pal) {
 
-  tibble::enframe(pal) |>
-    dplyr::mutate(example = "") |>
+  `%>%` <- dplyr::`%>%`
+
+  tibble::enframe(pal) %>%
+    dplyr::mutate(example = "") %>%
     dplyr::mutate(value = factor(
       pal,
       ordered = TRUE,
       levels = unname(pal)
-    )) |>
-    gt::gt() |>
+    )) %>%
+    gt::gt() %>%
     gt::data_color(
       columns = .data$value,
       target_columns = .data$example,
@@ -24,16 +26,16 @@ colour_table <- function(pal) {
         palette = pal,
         domain = NULL
       )
-    ) |>
+    ) %>%
     gt::cols_width(
       name ~ gt::px(110),
       value ~ gt::px(100),
       example ~ gt::px(150)
-    ) |>
+    ) %>%
     gt::cols_align(
       align = "left",
       columns = dplyr::everything()
-    ) |>
+    ) %>%
     gt::cols_label(
       name = "Colour name",
       value = "Hex code",
